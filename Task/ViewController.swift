46620a0550a6bb542595a4d311a49180163640c6
Haggag <>
2024-06-04 22:42:30 +0300

    init project

---
+++ Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Haggag on 04/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
